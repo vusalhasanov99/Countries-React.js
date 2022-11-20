c5dfd994d4f6843e3a0f22116143a78b79015359
Vusal Hasanov <vusqa1999@gmail.com>
2022-11-20 14:35:15 +0400

    Countries

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -3,34 +3,47 @@ import CoutryCard from './CountryCard';
 import axios from 'axios';
 
 
+
+
 export default class CountryList extends Component {
-  state={
-    countries:[]
+  state = {
+    countries: [],
+    limit:10
   }
-  componentDidMount(){
+
+  componentDidMount() {
     axios.get('https://restcountries.com/v2/all')
-    .then(rsp=>this.setState({countries:rsp.data})
+      .then(rsp => this.setState({ countries: rsp.data })
       )
-  }searchHandler=(e)=>{
-    axios.get('https://restcountries.com/v2/name/'+e.target.value)
-    .then(res=>this.setState({
-      countries:res.data
-    }))
+  } searchHandler = (e) => {
+    axios.get('https://restcountries.com/v2/name/' + e.target.value)
+      .then(res => this.setState({
+        countries: res.data
+      }))
   }
+  
+
   render() {
-        const countryList=this.state.countries.map(c=>{
-            return(
+    const countryList = this.state.countries.slice(0,this.state.limit).map(c => {
+      return (
+
+        <CoutryCard country={c} key={c.numericCode} />
         
-               <CoutryCard country={c} key={c.numericCode} />
-            )
-        })
-        return(
-            <div className='d-flex flex-wrap'>
-              <input type="text" className='form-control m-3' style={{width:"1000px"}} onChange={this.searchHandler} placeholder='Country Name' />
-              {countryList}
-              </div>
-        )
-    
-    
+      )
+    })
+    return (
+      <div style={{textAlign:"center"}}>
+      
+      <div className='d-flex flex-wrap'>
+        <input type="text" className='form-control m-3' style={{ width: "1000px" }} onChange={this.searchHandler} placeholder='Country Name' />
+        {countryList}
+      </div> 
+      {this.state.limit<250 && <button onClick={() => this.setState({ limit: this.state.limit + 100 }) }>Load More</button>}
+
+
+      </div>
+    )
+
+
   }
 }
